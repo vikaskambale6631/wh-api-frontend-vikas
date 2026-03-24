7143bb38969ba6c19ca8f8a9b2fd0fbc2a5ad985
vikas kambale <vikaskambale6631@gmail.com>
2026-03-24 20:56:28 +0530

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,13 +1,13 @@
-// export const API_BASE_URL =
-//   process.env.NEXT_PUBLIC_API_URL || "https://whatsapp-platfrom-backent1.onrender.com/api";
-
-// export const WHATSAPP_ENGINE_URL =
-//   process.env.NEXT_PUBLIC_WHATSAPP_ENGINE_URL || "https://whatsapp-platfrom-engine1.onrender.com";
-
-
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+  process.env.NEXT_PUBLIC_API_URL || "https://wh-api-backend-vikas.onrender.com//api";
 
 export const WHATSAPP_ENGINE_URL =
-  process.env.NEXT_PUBLIC_WHATSAPP_ENGINE_URL || "http://localhost:3002";
+  process.env.NEXT_PUBLIC_WHATSAPP_ENGINE_URL || "https://wh-api-whatsapp-engine-vikas.onrender.com/";
+
+
+// export const API_BASE_URL =
+//   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+
+// export const WHATSAPP_ENGINE_URL =
+//   process.env.NEXT_PUBLIC_WHATSAPP_ENGINE_URL || "http://localhost:3002";
 
